2ba69978e5199f81d7d7dd6a2c4ce9b8f9238ce1
CarolinaCedro <yotechgirl@gmail.com>
2022-08-11 16:07:56 -0300

    post

--- src/main/java/com/learningspring/course/resources/UserResource.java
+++ src/main/java/com/learningspring/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.learningspring.course.entities.User;
 import com.learningspring.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,6 +21,13 @@ public class UserResource {
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{i}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
     @GetMapping(value = "/{id}")
--- src/main/java/com/learningspring/course/services/UserService.java
+++ src/main/java/com/learningspring/course/services/UserService.java
@@ -26,4 +26,9 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){
+        return repository.save(obj);
+    }
+
 }
