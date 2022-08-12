2b071c34b7eed42044203180ce80dd57bcd5090b
CarolinaCedro <yotechgirl@gmail.com>
2022-08-11 21:38:33 -0300

    feat: execp

--- src/main/java/com/learningspring/course/services/UserService.java
+++ src/main/java/com/learningspring/course/services/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,9 +48,14 @@ public class UserService {
     }
 
     public User update(Long id,User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return  repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return  repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
